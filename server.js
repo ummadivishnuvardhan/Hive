4b510ac1b61e3cd0767d11adab10559625fc2a16
ummadivishnuvardhan <ummadivishnuvardhan46@gmail.com>
2026-03-17 14:02:44 +0530

    feat: add /profile, /about, and /contact API routes

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
     res.send('Hello World 🚀');
 });
 
+app.get('/profile', (req, res) => {
+    res.send('This is the profile page.');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page.');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('This is the contact page.');
+});
+
 const PORT = 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
